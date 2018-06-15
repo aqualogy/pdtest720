5d6c6d30d6c20355759a6a17e561d08f427d1b3d
Arnd Weber <arnd.weber@bafg.de>
2018-06-15 14:35:47 +0200

    initial commit

---
+++ R/data.R
@@ -0,0 +1,161 @@
+#' @name df.flys1
+#' @rdname df.flys1
+#' 
+#' @title Stationary water levels from the FLYS 3-database
+#' 
+#' @description This dataset contains the 30 stationary 1D water levels for the
+#'   rivers \strong{Elbe} and \strong{Rhein} originally stored in the 
+#'   \href{https://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html}{FLYS3}-database.
+#' 
+#' @format A \code{data.frame} with 4 columns: 
+#' \describe{
+#'   \item{river}{name of the relevant water body (type \code{character}).}
+#'   \item{name}{of the FLYS 3 water level (type \code{character}). See details for more information.}
+#'   \item{station}{rivers stationing (type \code{numeric}).}
+#'   \item{w}{water level (cm above gauging station null, type \code{numeric}).} 
+#' }
+#' 
+#' @details The \code{name}ing of the water levels is \code{river}-specific.
+#' 
+"df.flys1"
+
+#' @name df.flys2
+#' @rdname df.flys2
+#' 
+#' @title Stationary water levels from the FLYS 3-database
+#' 
+#' @description This dataset contains the 30 stationary 1D water levels for the
+#'   rivers \strong{Elbe} and \strong{Rhein} originally stored in the 
+#'   \href{https://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html}{FLYS3}-database.
+#' 
+#' @format A \code{data.frame} with 4 columns: 
+#' \describe{
+#'   \item{river}{name of the relevant water body (type \code{character}).}
+#'   \item{name}{of the FLYS 3 water level (type \code{character}). See details for more information.}
+#'   \item{station}{rivers stationing (type \code{numeric}).}
+#'   \item{w}{water level (cm above gauging station null, type \code{numeric}).} 
+#' }
+#' 
+#' @details The \code{name}ing of the water levels is \code{river}-specific.
+#' 
+#' @references
+#'   Busch, N., Hammer, M., 2009: Einheitliche Grundlage für die Festlegung der 
+#'   Bemessungswasserspiegellagen der Elbe auf der frei fließenden Strecke in 
+#'   Deutschland. BfG-Bericht 1650, Bundesanstalt für Gewässerkunde, Koblenz, 
+#'   Germany. DOI: 10.5675/bfg-1650
+#'   
+#'   HKV Hydrokontor, 2014: Erstellung eines SOBEK-River Modells für den Rhein 
+#'   von Iffezheim bis Pannerdense Kop als Weiterentwicklung bestehender 
+#'   SOBEK-RE Modelle. 
+#'   
+#'   Bundesanstalt für Gewässerkunde, 2016: FLYS – Flusshydrologischer 
+#'   Webdienst. URL: http://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html
+#'   
+#'   DELTARES, 2018: SOBEK. URL: https://www.deltares.nl/en/software/sobek
+#' 
+"df.flys2"
+
+#' @name df.flys3
+#' @rdname df.flys3
+#' 
+#' @title Stationary water levels from the FLYS 3-database
+#' 
+#' @description This dataset contains the 30 stationary 1D water levels for the
+#'   rivers \strong{Elbe} and \strong{Rhein} originally stored in the 
+#'   \href{https://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html}{FLYS3}-database.
+#' 
+#' @format A \code{data.frame} with 4 columns: 
+#' \describe{
+#'   \item{river}{name of the relevant water body (type \code{character}).}
+#'   \item{name}{of the FLYS 3 water level (type \code{character}). See details for more information.}
+#'   \item{station}{rivers stationing (type \code{numeric}).}
+#'   \item{w}{water level (cm above gauging station null, type \code{numeric}).} 
+#' }
+#' 
+#' @details The \code{name}ing of the water levels is \code{river}-specific.
+#' 
+#' @references
+#'   Busch, N., Hammer, M., 2009: Einheitliche Grundlage für die Festlegung der 
+#'   Bemessungswasserspiegellagen der Elbe auf der frei fließenden Strecke in 
+#'   Deutschland. BfG-Bericht 1650, Bundesanstalt für Gewässerkunde, Koblenz, 
+#'   Germany. DOI: 10.5675/bfg-1650
+#'   
+#'   HKV Hydrokontor, 2014: Erstellung eines SOBEK-River Modells für den Rhein 
+#'   von Iffezheim bis Pannerdense Kop als Weiterentwicklung bestehender 
+#'   SOBEK-RE Modelle. 
+#'   
+#'   Bundesanstalt für Gewässerkunde, 2016: FLYS – Flusshydrologischer 
+#'   Webdienst. URL: \url{http://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html}
+#'   
+#'   DELTARES, 2018: SOBEK. URL: \url{https://www.deltares.nl/en/software/sobek}
+#' 
+"df.flys3"
+
+#' @name df.flys4
+#' @rdname df.flys4
+#' 
+#' @title Stationary water levels from the FLYS 3-database
+#' 
+#' @description This dataset contains the 30 stationary 1D water levels for the
+#'   rivers \strong{Elbe} and \strong{Rhein} originally stored in the 
+#'   \href{https://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html}{FLYS3}-database.
+#' 
+#' @format A \code{data.frame} with 4 columns: 
+#' \describe{
+#'   \item{river}{name of the relevant water body (type \code{character}).}
+#'   \item{name}{of the FLYS 3 water level (type \code{character}). See details for more information.}
+#'   \item{station}{rivers stationing (type \code{numeric}).}
+#'   \item{w}{water level (cm above gauging station null, type \code{numeric}).} 
+#' }
+#' 
+#' @details The \code{name}ing of the water levels is \code{river}-specific.
+#' 
+#' @references
+#'   Busch, N., Hammer, M., 2009: Einheitliche Grundlage für die Festlegung der 
+#'   Bemessungswasserspiegellagen der Elbe auf der frei fließenden Strecke in 
+#'   Deutschland. BfG-Bericht 1650, Bundesanstalt für Gewässerkunde, Koblenz, 
+#'   Germany. DOI: \doi{10.5675/bfg-1650}
+#'   
+#'   HKV Hydrokontor, 2014: Erstellung eines SOBEK-River Modells für den Rhein 
+#'   von Iffezheim bis Pannerdense Kop als Weiterentwicklung bestehender 
+#'   SOBEK-RE Modelle. 
+#'   
+#'   Bundesanstalt für Gewässerkunde, 2016: FLYS – Flusshydrologischer 
+#'   Webdienst. URL: \url{http://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html}
+#'   
+#'   DELTARES, 2018: SOBEK. URL: \url{https://www.deltares.nl/en/software/sobek}
+#' 
+"df.flys4"
+
+#' @name df.flys5
+#' @rdname df.flys5
+#' 
+#' @title Stationary water levels from the FLYS 3-database
+#' 
+#' @description This dataset contains the 30 stationary 1D water levels for the
+#'   rivers \strong{Elbe} and \strong{Rhein} originally stored in the 
+#'   \href{https://www.bafg.de/DE/08_Ref/M2/03_Fliessgewmod/01_FLYS/flys_node.html}{FLYS3}-database.
+#'   
+#'   \if{html}{\figure{flys3waterlevels.png}{options: width="60\%" alt="Figure: flys3waterlevels.png"}}
+#'   \if{latex}{\figure{flys3waterlevels.pdf}{options: width=7cm}}
+#' 
+#' @format A \code{data.frame} with 4 columns: 
+#' \describe{
+#'   \item{river}{name of the relevant water body (type \code{character}).}
+#'   \item{name}{of the FLYS 3 water level (type \code{character}). See details for more information.}
+#'   \item{station}{rivers stationing (type \code{numeric}).}
+#'   \item{w}{water level (cm above gauging station null, type \code{numeric}).} 
+#' }
+#' 
+#' @details The \code{name}ing of the water levels is \code{river}-specific.
+#' 
+#' @references
+#'   \insertRef{busch_einheitliche_2009}{pdtest720}
+#'   
+#'   \insertRef{hkv_hydrokontor_erstellung_2014}{pdtest720}
+#'   
+#'   \insertRef{bundesanstalt_fur_gewasserkunde_flys_2016}{pdtest720}
+#'   
+#'   \insertRef{deltares_sobek_2018}{pdtest720}
+#' 
+"df.flys5"
